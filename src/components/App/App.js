557102a6ff67d5f749fa5b8080189b0422c7428d
ConorLeckey <cgleckey@gmail.com>
2023-12-24 18:06:34 +0000

    Reorganise dice components
    Add InputSpinner package

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,57 +1,25 @@
 import './App.css';
-import Dice from "../Dice/Dice";
-import React, {useEffect, useState} from "react";
-import Button from "react-bootstrap/Button";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faMinus, faPlus} from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import DiceRoller from "../DiceRoller/DiceRoller";
+import {Fade, Tab, Tabs} from "react-bootstrap";
 
 function App() {
-    const [dice, setDice] = useState([1, 2, 1]);
-
-    useEffect(() => {
-    }, [dice]);
-
-    function randomiseDice(diceArray) {
-        const newDice = []
-        for (let i = 0; i < diceArray.length; i++) {
-            newDice[i] = Math.floor(Math.random() * 6) + 1
-        }
-        return newDice
-    }
-
-    function rollDice() {
-        setDice(randomiseDice(dice))
-    }
-    function changeDiceAmount(difference) {
-        const newDice = Array(dice.length + difference)
-        console.log(difference)
-        console.log(newDice)
-        setDice(randomiseDice(newDice))
-    }
-
     return (
         <div className="App">
-            {/*<FontAwesomeIcon className="burger-icon fa-2x" icon={faPlus} />*/}
-            <div className="diceplay">
-                {dice.map(die => <Dice number={die}/>)}
-            </div>
-            <div className={"control"}>
-                <Button variant={"light"}
-                        className="btn-change-dice"
-                        disabled={dice.length === 1}
-                        onClick={()=>changeDiceAmount(-1)}
-                >
-                    <FontAwesomeIcon className="burger-icon fa-2x" icon={faMinus} />
-                </Button>
-                <Button variant={"outline-light"} className="btn-roll" onClick={rollDice}>Roll!</Button>
-                <Button variant={"light"}
-                        className="btn-change-dice"
-                        disabled={dice.length === 3}
-                        onClick={()=>changeDiceAmount(1)}
-                >
-                    <FontAwesomeIcon className="burger-icon fa-2x" icon={faPlus} />
-                </Button>
-            </div>
+            <Tabs
+              defaultActiveKey="roller"
+              id="uncontrolled-tab-example"
+              transition={Fade}
+              className="mb-3"
+              justify
+            >
+              <Tab eventKey="roller" title="Dice Roller">
+                 <DiceRoller/>
+              </Tab>
+              <Tab eventKey="blitz" title="Blitz">
+
+              </Tab>
+            </Tabs>
         </div>
     );
 }
---
+++ src/components/DiceRoller/DiceRoller.js
@@ -0,0 +1,58 @@
+import './DiceRoller.css';
+import Dice from "./Dice/Dice";
+import React, {useEffect, useState} from "react";
+import Button from "react-bootstrap/Button";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faMinus, faPlus} from "@fortawesome/free-solid-svg-icons";
+
+function DiceRoller() {
+    const [dice, setDice] = useState([1, 2, 1]);
+
+    useEffect(() => {
+    }, [dice]);
+
+    function randomiseDice(diceArray) {
+        const newDice = []
+        for (let i = 0; i < diceArray.length; i++) {
+            newDice[i] = Math.floor(Math.random() * 6) + 1
+        }
+        return newDice
+    }
+
+    function rollDice() {
+        setDice(randomiseDice(dice))
+    }
+    function changeDiceAmount(difference) {
+        const newDice = Array(dice.length + difference)
+        console.log(difference)
+        console.log(newDice)
+        setDice(randomiseDice(newDice))
+    }
+
+    return (
+        <div className="dice-roller">
+            <div className="diceplay">
+                {dice.map(die => <Dice number={die}/>)}
+            </div>
+            <div className={"control"}>
+                <Button variant={"light"}
+                        className="btn-change-dice"
+                        disabled={dice.length === 1}
+                        onClick={()=>changeDiceAmount(-1)}
+                >
+                    <FontAwesomeIcon className="burger-icon fa-2x" icon={faMinus} />
+                </Button>
+                <Button variant={"outline-light"} className="btn-roll" onClick={rollDice}>Roll!</Button>
+                <Button variant={"light"}
+                        className="btn-change-dice"
+                        disabled={dice.length === 3}
+                        onClick={()=>changeDiceAmount(1)}
+                >
+                    <FontAwesomeIcon className="burger-icon fa-2x" icon={faPlus} />
+                </Button>
+            </div>
+        </div>
+    );
+}
+
+export default DiceRoller;
